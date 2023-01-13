b5a8afa7239d840ca9e07b37e0b3bfa2a36667f3
abusohib <abushoaib73@gmail.com>
2023-01-13 23:07:08 +0600

    Mytask model with migration added

---
+++ database/migrations/2023_01_13_165206_create_mytasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mytasks', function (Blueprint $table) {
+            $table->id();
+            $table->string("taskTitle",100);
+            $table->string("taskDescription",200);
+            $table->smallInteger("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mytasks');
+    }
+};
